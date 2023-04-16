eca1fc3b54a804dd64b6703e4c57202f7b286c8e
etobaza <rblxxmartin@gmail.com>
2023-04-16 11:17:39 +0600

    My DB settings (Martin)

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -47,6 +49,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:4200",  # Angular frontend
 ]
 
 ROOT_URLCONF = 'backend.urls'
