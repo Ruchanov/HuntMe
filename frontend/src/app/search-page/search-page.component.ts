11c85b90f052edb77c515172f2f37eda6ff46b72
etobaza <rblxxmartin@gmail.com>
2023-04-28 10:22:26 +0600

    Minor fixes to search

--- frontend/src/app/search-page/search-page.component.ts
+++ frontend/src/app/search-page/search-page.component.ts
@@ -9,32 +9,42 @@ import {Router} from "@angular/router";
   styleUrls: ['./search-page.component.css'],
 })
 export class SearchPageComponent implements OnInit {
-  searchResults: JobListing[] = []; // Use the JobListing interface
-
+  searchResults: JobListing[] = [];
+  originalResults: JobListing[] = [];
   constructor(private jobService: JobService, private router: Router) {}
 
   ngOnInit(): void {
     this.jobService.fetchJobListings().subscribe({
       next: (jobListings) => {
         this.searchResults = jobListings;
+        this.originalResults = jobListings;
       },
       error: (error) => {
         console.error('Error fetching vacancies:', error);
       },
     });
   }
+
   navigateToJobDetails(id: string) {
     this.router.navigate(['/vacancy', id]);
   }
+
   search(): void {
     const query = (document.getElementById('search-bar') as HTMLInputElement).value.toLowerCase();
-    this.searchResults = this.searchResults.filter((jobListing) => {
+    this.searchResults = this.originalResults.filter((jobListing) => {
+      const title = jobListing.title ? jobListing.title.toLowerCase() : '';
+      const companyName = jobListing.company && jobListing.company.name ? jobListing.company.name.toLowerCase() : '';
+      const description = jobListing.description ? jobListing.description.toLowerCase() : '';
+
       return (
-        jobListing.title.toLowerCase().includes(query) ||
-        jobListing.company.name.toLowerCase().includes(query) ||
-        jobListing.description.toLowerCase().includes(query)
+        title.includes(query) ||
+        companyName.includes(query) ||
+        description.includes(query)
       );
     });
   }
+
+
+
 
 }
